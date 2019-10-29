652ac988e18175878e44acb5c2ffb988742dfe8b
Prem Kumar <prmsrswt@gmail.com>
2019-10-29 10:37:48 +0530

    added correct logos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <img src="/assets/logo.png" className="logo" alt="Aurora logo" />
+    <div className={styles.App}>
+      <img src="/assets/logo.svg" className={styles.logo} alt="Aurora logo" />
     </div>
   );
 }
