630fc7276307fd421c926c85e923357412c81638
Prem Kumar <prmsrswt@gmail.com>
2019-10-29 09:56:12 +0530

    added eslint+prettier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<img src="/assets/logo.png" className="logo" alt="Aurora logo"/>
-		</div>
-	);
+  return (
+    <div className="App">
+      <img src="/assets/logo.png" className="logo" alt="Aurora logo" />
+    </div>
+  );
 }
 
 export default App;
