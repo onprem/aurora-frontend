5e915bb8177e0473cce982b31ddca84678fa745d
Prashant <prashantk.tps@gmail.com>
2019-10-31 17:38:11 +0530

    add nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
 
+import Nav from './views/nav/Nav';
+
 function App() {
   return (
     <div className={styles.App}>
-      <img src="/assets/logo.svg" className={styles.logo} alt="Aurora logo" />
+      {/* <img src="/assets/logo.svg" className={styles.logo} alt="Aurora logo" /> */}
+      <Nav />
     </div>
   );
 }
