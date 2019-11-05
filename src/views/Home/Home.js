83d047673df98d14ab152f710d66ad69ac06b0d0
Prem Kumar <prmsrswt@gmail.com>
2019-11-06 02:36:29 +0530

    added aurora branding, modified animation

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Graveyard from '../../components/graveyard/Graveyard';
-import { ReactComponent as AuroraMonoIcon } from '../../assets/icons/auroraMono.svg';
+import { ReactComponent as AuroraCircleIcon } from '../../assets/icons/auroraCircle.svg';
+import { ReactComponent as AuroraTextIcon } from '../../assets/icons/auroraText.svg';
 import styles from './Home.module.css';
 
 const Home = () => {
   return (
     <div className={styles.Home}>
-      <AuroraMonoIcon width="25vh" />
+      <AuroraCircleIcon width="25vh" className={styles.Logo} />
+      <AuroraTextIcon width="30vh" />
       <Graveyard />
     </div>
   );
