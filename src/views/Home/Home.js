8e00ac90781cd7a1b1c985cd1ff93505d6262424
Prem Kumar <prmsrswt@gmail.com>
2019-11-06 05:08:40 +0530

    add useMediaQuery hook

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Parallax } from 'react-scroll-parallax';
 import Graveyard from '../../components/graveyard/Graveyard';
+import useMediaQuery from '../../utils/useMediaQuery';
 import { ReactComponent as AuroraCircleIcon } from '../../assets/icons/auroraCircle.svg';
 import { ReactComponent as AuroraTextIcon } from '../../assets/icons/auroraText.svg';
 import styles from './Home.module.css';
 
 const Home = () => {
+  const isMobile = useMediaQuery('(min-width: 450px)');
   return (
     <>
       <div className={styles.Home}>
-        <Parallax y={[-110, 75]}>
-          <AuroraCircleIcon className={styles.Logo} />
-        </Parallax>
-        <Parallax y={[-160, 380]}>
-          <AuroraTextIcon className={styles.Text} />
-        </Parallax>
+        {isMobile ? (
+          <>
+            <Parallax y={[-110, 75]}>
+              <AuroraCircleIcon className={styles.Logo} />
+            </Parallax>
+            <Parallax y={[-160, 380]}>
+              <AuroraTextIcon className={styles.Text} />
+            </Parallax>
+          </>
+        ) : (
+          <>
+            <AuroraCircleIcon className={styles.Logo} />
+            <AuroraTextIcon className={styles.Text} />
+          </>
+        )}
         <Graveyard />
       </div>
       <div className={styles.Footer}>
