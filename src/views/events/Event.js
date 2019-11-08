6d7789909feee22ae1d4d7741a2940c5294c7726
prashant <prashantk.tps@gmail.com>
2019-11-09 00:15:56 +0530

    add contact page

--- src/views/events/Event.js
+++ src/views/events/Event.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import style from './event.module.css';
 
 import Card from '../../components/card/Card';
 import Carousel from '../../components/Carousel/Carousel';
@@ -6,10 +9,14 @@ import Particles from '../../components/particles/Particle';
 import Social from '../../components/Social/Social';
 
 import cardData from '../../assets/data/eventData/eventdata';
+import Logo from '../../assets/icons/auroraLogo.svg';
 
 const Event = () => {
   return (
     <>
+      <Link to="/>">
+        <img className={style.aurora_logo} src={Logo} alt="logo" />
+      </Link>
       <Carousel>
         {cardData.map(event => (
           <Card params={event} />
