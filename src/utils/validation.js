04c1172d20b2f1d8dd3b28160ec9a308030fd8e9
Prem Kumar <prmsrswt@gmail.com>
2019-11-25 00:46:10 +0530

    Revert "Login page"

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,17 +1,6 @@
-export const emailValidation = email => {
+const emailValidation = email => {
   const regex = /^(?=[A-Za-z0-9][A-Za-z0-9@._%+-]{5,253}$)[A-Za-z0-9._%+-]{1,64}@(?:(?=[A-Za-z0-9-]{1,63}\.)[A-Za-z0-9]+(?:-[A-Za-z0-9]+)*\.){1,8}[A-Za-z]{2,5}$/;
   const res = regex.test(email);
   return res;
 };
-
-export const ARValidation = ar => {
-  const regex = /[a][r]-[a-z][a-z][a-z]-[0-9][0-9][0-9]/i;
-  const res = regex.test(ar);
-  return res;
-};
-
-export const phoneValidation = phone => {
-  const regex = /[6-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/;
-  const res = regex.test(phone);
-  return res;
-};
+export default emailValidation;
