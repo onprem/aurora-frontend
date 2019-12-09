a13ad48961f6f4c2e6b3182a02b9e238d0854466
Prashant Kushwaha <prashantk.tps@gmail.com>
2019-12-09 20:29:33 +0000

    add confirmation modal on pay button

--- src/components/Dashboard/Events/PayButton/PayButton.js
+++ src/components/Dashboard/Events/PayButton/PayButton.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useMutation, useQuery } from '@apollo/react-hooks';
 
+import Swal from 'sweetalert2/src/sweetalert2';
+
 import Loader from '../../../Loader/Loader';
 import useRzrPay from '../../../../utils/useRzrPay';
 import getAlert from '../../../../utils/getAlert';
@@ -80,12 +82,24 @@ const PayButton = ({ toPay, setToPay }) => {
 
   const totalMoney = toPay.reduce((accumulator, team) => accumulator + team.event.fee, 0);
   const teamIds = toPay.map(team => team.id);
+  const placeOrder = () => {
+    Swal.fire({
+      title: 'Are you sure?',
+      html: `<ul><li style="text-align:left">Total Amount to be payed is ${totalMoney} + payment gateway charges</li><li style="text-align:left"><p style="text-align:left">Terms and Conditions</p><ul><li style="text-align:left; margin-bottom:10px">Users can be added to the team but cannot be removed or cannot leave the team once the payment for the team is done</li><li style="text-align:left; margin-bottom:10px">Payment is non-refundable under any circumstances whatsoever except for extreme cases. Contact our tech support team(<a style="text-decoration:underline" href="mailto:support@aurorafest.org">support@aurorafest.org</a>) for extreme case scenarios.</li></ul></li><li style="text-align:left">By continuing to payment you abide by all the terms and conditions listed above</li></ul>`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'white',
+      cancelButtonColor: 'white',
+      cancelButtonText: '<span style="color:black">Cancel</span>',
+      confirmButtonText: '<span style="color:black">PAY</span>',
+    }).then(result => {
+      if (result.value) {
+        getOrder({ variables: { teamIds } });
+      }
+    });
+  };
   return (
-    <button
-      type="button"
-      className={styles.PayBtn}
-      onClick={() => getOrder({ variables: { teamIds } })}
-    >
+    <button type="button" className={styles.PayBtn} onClick={placeOrder}>
       {loading ? (
         <Loader height="1em" />
       ) : (
