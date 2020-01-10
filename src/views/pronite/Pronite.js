8215262f6ce1c9ed64cb5219c8b4cbf88fb4e36b
Prashant <prashantk.tps@gmail.com>
2020-01-10 16:27:12 +0530

    pronite 1st

--- src/views/pronite/Pronite.js
+++ src/views/pronite/Pronite.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import style from './pronite.module.css';
+
+import useMediaQuery from '../../utils/useMediaQuery';
 
 import Particles from '../../components/particles/Particle';
 
-import hero from '../../assets/images/IO.jpg';
+import hero from '../../assets/images/aceaxe$tog.png';
+
+import { ReactComponent as Logo } from '../../assets/icons/auroraMono.svg';
+import { ReactComponent as Arrow } from '../../assets/icons/arrowLeft.svg';
+import Social from '../../components/Social/Social';
 
 const strokeCount = 35;
 
@@ -13,8 +21,8 @@ const ProniteCard = ({ name }) => {
     <div className={style.pronite_card}>
       <div className={style.pronite_left_container}>
         <div className={style.pronite_stroke_container}>
-          {[...Array(strokeCount)].map((e, i) => (
-            <div className={style.pronite_vertical} key={i} />
+          {[...Array(strokeCount)].map(e => (
+            <div className={style.pronite_vertical} key={e} />
           ))}
         </div>
         <div className={style.pronite_img_container}>
@@ -28,8 +36,8 @@ const ProniteCard = ({ name }) => {
         </div>
       </div>
       <div className={style.pronite_right_container}>
-        <h1 className={style.pronite_heading}>THE SUBLIME</h1>
-        <h2 className={style.pronite_sub_heading}>RITVIZ</h2>
+        <h1 className={style.pronite_heading}>THE MAGICAL</h1>
+        <h2 className={style.pronite_sub_heading}>OLLY ESSE & ACEAXE</h2>
         <p className={style.pronite_p}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -43,13 +51,32 @@ const ProniteCard = ({ name }) => {
   );
 };
 
+// eslint-disable-next-line no-unused-vars
+const RenderButtons = () => {
+  return (
+    <div className={style.button_parent_container}>
+      <button className={style.next_prev_button} type="button">
+        <Arrow fill="black" className={style.arrow_left} />
+      </button>
+      <button className={style.next_prev_button} type="button">
+        <Arrow fill="black" className={style.arrow_right} />
+      </button>
+    </div>
+  );
+};
+
 const Pronite = () => {
   return (
     <>
       <div className={style.pronite_parent}>
+        <Link to="/">
+          <Logo className={style.logoDark} />
+        </Link>
         <ProniteCard />
+        {/* <RenderButtons /> */}
       </div>
       <Particles />
+      {useMediaQuery(`(max-width:1000px)`) ? null : <Social fill="black" />}
     </>
   );
 };
