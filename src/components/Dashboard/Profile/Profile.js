c2abda7bff914ccaa2d7b138782d5904c455142f
Prem Kumar <prmsrswt@gmail.com>
2020-01-15 19:44:51 +0530

    enable changing display picture

--- src/components/Dashboard/Profile/Profile.js
+++ src/components/Dashboard/Profile/Profile.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classNames from 'classnames';
+import { useMutation } from '@apollo/react-hooks';
+import getAlert from '../../../utils/getAlert';
+
+import UPLOAD_PHOTO from '../../../graphQl/mutations/uploadPhoto';
+import { ReactComponent as EditIcon } from '../../../assets/icons/edit.svg';
 
 import config from '../../../config';
 
@@ -7,12 +12,57 @@ import styles from './Profile.module.css';
 
 const Profile = ({ className, user }) => {
   const dpBaseURL = config.gcsBucketUrl;
+  const dpRef = useRef(null);
+
+  const handleErrors = error => {
+    if (error && error.graphQLErrors.length > 0) {
+      const toast = getAlert();
+      toast.fire({
+        icon: 'error',
+        title: error.graphQLErrors[0].message,
+      });
+    }
+  };
+
+  const [upload, { loading }] = useMutation(UPLOAD_PHOTO, {
+    onCompleted: data => {
+      const toast = getAlert();
+      toast.fire({
+        icon: 'success',
+        title: data.uploadPhoto.message,
+      });
+      setTimeout(() => {
+        dpRef.current.src = dpBaseURL + data.uploadPhoto.user.displayPic;
+      }, 2000);
+    },
+    onError: handleErrors,
+  });
+
+  const handleChange = event => {
+    const file = event.target.files[0];
+    upload({ variables: { photo: file } });
+  };
+  const up = () => {
+    const chBtn = document.getElementById('chBtn');
+    chBtn.click();
+  };
 
   return (
     <section className={classNames(styles.profileSection, className)}>
       <div className={styles.leftDiv}>
         <div className={styles.photoDiv}>
-          <img src={dpBaseURL + user.displayPic} alt="Profile" />
+          <img ref={dpRef} src={dpBaseURL + user.displayPic} alt="Profile" />
+          <input
+            id="chBtn"
+            className={styles.changePhotoBtn}
+            type="file"
+            name="photo"
+            onChange={handleChange}
+            disabled={loading}
+          />
+          <button type="button" className={styles.edBtn} onClick={up} disabled={loading}>
+            <EditIcon height="2em" />
+          </button>
         </div>
         <div className={styles.nameDiv}>
           <span>{user.name}</span>
