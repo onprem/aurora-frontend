befc30a300b68f55c3f94acd4097831b4082d0cc
Prashant Kushwaha <prashantk.tps@gmail.com>
2020-01-27 18:54:49 +0000

    registrations closed

--- src/assets/data/eventData/notificationData.js
+++ src/assets/data/eventData/notificationData.js
@@ -1,5 +1,2 @@
-const notificationData = [
-  'Due to heavy participation, event registrations are going to be closed from 28th January 2020 at 00:00',
-  'Due to heavy participation, event registration for Corna will be closed soon.',
-];
+const notificationData = ['Due to heavy participation, event registrations are closed.'];
 export default notificationData;
