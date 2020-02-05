64cc14be3ccf924724d741242c9ba0d2c468260c
Prem Kumar <prmsrswt@gmail.com>
2020-02-05 13:04:19 +0530

    show sponsors instead of past sponsors

---
+++ src/assets/data/sponsorData.js
@@ -0,0 +1,90 @@
+import cocaCola from '../images/sponsors/cocacola.png';
+import tribeVibe from '../images/sponsors/tribevibe.png';
+import yonoSbi from '../images/sponsors/yono-sbi.png';
+import eurofins from '../images/sponsors/eurofins.jpg';
+import inox from '../images/sponsors/inox.png';
+import pnb from '../images/sponsors/pnb.png';
+// import gwlSmartCity from '../images/sponsors/gwl-smart-city.jpg';
+// import yesBank from '../images/sponsors/yes-bank.png';
+import dalmia from '../images/sponsors/dalmia.png';
+// import nagarNigam from '../images/sponsors/nagar-nigam.png';
+// import bigFM from '../images/sponsors/bigfm.png';
+// import lic from '../images/sponsors/lic.png';
+import atkt from '../images/sponsors/atkt.png';
+// import indusInd from '../images/sponsors/indusind.jpg';
+// import mmt from '../images/sponsors/mmt.png';
+import sehatOne from '../images/sponsors/sehatone.png';
+// import mpTourism from '../images/sponsors/mp-tourism.png';
+import sics from '../images/sponsors/sics.jpg';
+import boi from '../images/sponsors/boi.png';
+
+const titleSponsor = {
+  img: cocaCola,
+  name: 'Coca-Cola',
+  title: 'Title Sponsor',
+  url: 'https://www.coca-cola.com',
+};
+
+const sponsors = [
+  {
+    img: tribeVibe,
+    name: 'TribeVibe',
+    title: 'Festival Partner',
+    url: 'http://tribevibe.live',
+  },
+  {
+    img: eurofins,
+    name: 'Eurofins Clinical Genetics',
+    title: 'HealthCare Partner',
+    url: 'https://www.eurofins.com',
+  },
+  {
+    img: inox,
+    name: 'INOX',
+    title: 'Cinema Partner',
+    url: 'https://www.inoxmovies.com',
+  },
+  {
+    img: sehatOne,
+    name: 'SehatOne',
+    title: 'HealthCare Partner',
+    url: 'https://sehatone.com',
+  },
+  {
+    img: yonoSbi,
+    name: 'YONO - SBI',
+    title: 'Flagship Event Sponsor: MnM',
+    url: 'https://www.sbi.co.in',
+  },
+  {
+    img: dalmia,
+    name: 'Dalmia Group',
+    title: 'Foundation Partner',
+    url: 'https://www.dalmiabharat.com',
+  },
+  {
+    img: boi,
+    name: 'BOI',
+    title: 'Banking Partner',
+    url: 'https://bankofindia.co.in',
+  },
+  {
+    img: pnb,
+    name: 'PNB',
+    title: 'Banking Partner',
+    url: 'https://www.pnbindia.in',
+  },
+  {
+    img: sics,
+    name: 'SICS',
+    title: 'Event Sponsor: StepUp',
+    url: 'http://sicsindia.com',
+  },
+  {
+    img: atkt,
+    name: 'ATKT',
+    title: 'Sponsor',
+    url: 'https://atkt.in',
+  },
+];
+export default { titleSponsor, sponsors };
--- src/views/sponsors/Sponsors.js
+++ src/views/sponsors/Sponsors.js
@@ -4,18 +4,20 @@ import { Link } from 'react-router-dom';
 
 import style from './sponsors.module.css';
 
-import sponsors from '../../assets/data/eventData/sponsorData';
+import sponsorData from '../../assets/data/sponsorData';
 
 import Particles from '../../components/particles/Particle';
 import Social from '../../components/Social/Social';
 import useMediaQuery from '../../utils/useMediaQuery';
 import { ReactComponent as LogoDark } from '../../assets/icons/auroraMono.svg';
 
-const SponsorCard = ({ img, name, title }) => {
+const SponsorCard = ({ img, name, title, url }) => {
   return (
     <div className={style.sponsor_card_parent_container}>
       <div className={style.sponsor_img_container}>
-        <img className={style.sponsor_card_img} src={img} alt={name} />
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <img className={style.sponsor_card_img} src={img} alt={name} />
+        </a>
       </div>
       <h2 className={style.sponsor_heading}>{name}</h2>
       <span className={style.sponsor_desc}>{title}</span>
@@ -27,14 +29,24 @@ const Sponsors = () => {
   return (
     <>
       <div className={style.sponsor_parent_container}>
-        <h1 className={style.sponsor_heading_h1}>PAST SPONSORS</h1>
+        <h1 className={style.sponsor_heading_h1}>SPONSORS</h1>
         <div className={style.sponsor_section}>
-          {sponsors.map(sponsor => (
+          <SponsorCard
+            img={sponsorData.titleSponsor.img}
+            key={sponsorData.titleSponsor.img}
+            name={sponsorData.titleSponsor.name}
+            title={sponsorData.titleSponsor.title}
+            url={sponsorData.titleSponsor.url}
+          />
+        </div>
+        <div className={style.sponsor_section}>
+          {sponsorData.sponsors.map(sponsor => (
             <SponsorCard
               img={sponsor.img}
               key={sponsor.img}
               name={sponsor.name}
               title={sponsor.title}
+              url={sponsor.url}
             />
           ))}
         </div>
