febdf6a6490e56fc21e47e29a98c03e6cae73b6c
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-02-13 15:20:38 +0530

    close graffathon notif

--- src/assets/data/eventData/notificationData.js
+++ src/assets/data/eventData/notificationData.js
@@ -1,6 +1,5 @@
 const notificationData = [
-
-  'Event registration for Raag, Corna, Open Mic, Dancing Dyad and Beat the Beat has been closed.',
+  'Event registration for Raag, Corna, Open Mic, Graffathon Dancing Dyad and Beat the Beat has been closed.',
   'Schedule has been uploaded, please check from navbar',
 ];
 export default notificationData;
