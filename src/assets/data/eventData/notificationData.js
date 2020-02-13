fa30baa39a1c5e691f43f3e149562c7fc535580e
Prashant <prashantk.tps@gmail.com>
2020-02-13 21:58:08 +0530

    closed

--- src/assets/data/eventData/notificationData.js
+++ src/assets/data/eventData/notificationData.js
@@ -1,5 +1,5 @@
 const notificationData = [
-  'Event registration for Raag, Corna, Open Mic, Graffathon, Dancing Dyad and Beat the Beat has been closed.',
+  'Event registrations for all the events are closed.',
   'Schedule has been uploaded!',
 ];
 export default notificationData;
