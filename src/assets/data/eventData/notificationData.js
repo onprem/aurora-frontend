ad14747858a1988c88c1074f5db8a08c229eb88d
Prashant <prashantk.tps@gmail.com>
2020-02-16 21:13:14 +0530

    closed pronite

--- src/assets/data/eventData/notificationData.js
+++ src/assets/data/eventData/notificationData.js
@@ -1,4 +1,5 @@
 const notificationData = [
+  'Registration for pronite and accomodation has been closed.',
   'Schedule has been uploaded!',
   'Collect your bands soon, gate will be closed at 6:00 pm',
 ];
